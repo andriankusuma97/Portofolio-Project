5eec52ac824cd058112c79b3fcf455408fee1c93
andriankusuma97 <andriankusuma097@gmail.com>
2023-04-30 15:05:22 +0800

    layout: navbar layouting with tailwind

--- client-portofolio/src/components/Navbar.jsx
+++ client-portofolio/src/components/Navbar.jsx
@@ -1,21 +1,27 @@
 import { Link } from "react-router-dom";
-
+import { BsGithub,BsLinkedin } from "react-icons/bs";
 
 export default function Navbar(){
   return (
-    <div>
-      <h1>Navbar</h1>
-      <ul className="menu menu-horizontal px-1">
-          <li>
-            <Link to={"/"}>Utama</Link>
-          </li>
-          <li>
-            <Link to={"/about"}>About</Link>
-          </li>
-          <li>
-            <Link to={"/portofolio"}>Portofolio Projects</Link>
-          </li>
-        </ul>
+    <div className="flex justify-between py-10 mb-10 mx-10">
+      <div > 
+        <ul className="flex items-center font-mono text-2xl " >
+            <li className="mx-3 ">
+              <Link to={"/"}> <img className="rounded-full" width={40} height={40} src="https://avatars.githubusercontent.com/u/122440573?v=4" alt="" /> </Link>
+            </li>
+            <li className="mx-3 focus:cursor-auto hover:bg-slate-200 p-1 rounded-md">
+              <Link to={"/about"}>About</Link>
+            </li>
+            <li className="mx-3 focus:cursor-auto hover:bg-slate-200 p-1 rounded-md">
+              <Link to={"/portofolio"}>Projects</Link>
+            </li>
+          </ul>
+      </div>
+      <div className="flex">
+        <a href="https://github.com/andriankusuma97" className="text-3xl hover:text-4xl"> <BsGithub/> </a>
+        <a href="https://www.linkedin.com/in/andrian-kusuma-9150461a2/" className="text-3xl ml-3 hover:text-4xl"> <BsLinkedin/> </a>
+        
+      </div>
     </div>
   )
 }
--- client-portofolio/src/pages/HomePage.jsx
+++ client-portofolio/src/pages/HomePage.jsx
@@ -9,6 +9,7 @@ function HomePage() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">HELLLO WORLD</h1>
+      <img src="https://png.pngtree.com/png-clipart/20230102/original/pngtree-3d-boy-head-portrait-png-image_8855190.png" alt="" height={300} width={300}/>
     </div>
   );
 }
