29c64879f47c73a30efbd09846add0a87507c763
andriankusuma97 <andriankusuma097@gmail.com>
2023-04-30 21:31:23 +0800

    layout:home Page layouting with tailwind css

--- client-portofolio/src/components/Navbar.jsx
+++ client-portofolio/src/components/Navbar.jsx
@@ -1,25 +1,40 @@
 import { Link } from "react-router-dom";
 import { BsGithub,BsLinkedin } from "react-icons/bs";
+import { motion } from "framer-motion"
 
 export default function Navbar(){
   return (
-    <div className="flex justify-between py-10 mb-10 mx-10">
+    <div className="flex justify-between py-10 px-10 mb-4 ">
       <div > 
         <ul className="flex items-center font-mono text-2xl " >
-            <li className="mx-3 ">
+            <motion.li className="mx-3 "
+            whileHover={{ y: -3 }}
+            whileTap={{ scale: 0.9 }}
+            >
               <Link to={"/"}> <img className="rounded-full" width={40} height={40} src="https://avatars.githubusercontent.com/u/122440573?v=4" alt="" /> </Link>
-            </li>
-            <li className="mx-3 focus:cursor-auto hover:bg-slate-200 p-1 rounded-md">
+            </motion.li>
+            <motion.li className="mx-3 focus:cursor-auto hover:bg-slate-300 p-1 rounded-md"
+            whileHover={{ y: -3 }}
+            whileTap={{ scale: 0.9 }}>
               <Link to={"/about"}>About</Link>
-            </li>
-            <li className="mx-3 focus:cursor-auto hover:bg-slate-200 p-1 rounded-md">
-              <Link to={"/portofolio"}>Projects</Link>
-            </li>
+            </motion.li>
+            <motion.li className="mx-3 focus:cursor-auto hover:bg-slate-300 p-1 rounded-md"
+            whileHover={{ y: -3 }}
+            whileTap={{ scale: 0.9 }}>
+              <Link to={"/project"}>Projects</Link>
+            </motion.li>
           </ul>
       </div>
       <div className="flex">
-        <a href="https://github.com/andriankusuma97" className="text-3xl hover:text-4xl"> <BsGithub/> </a>
-        <a href="https://www.linkedin.com/in/andrian-kusuma-9150461a2/" className="text-3xl ml-3 hover:text-4xl"> <BsLinkedin/> </a>
+        <motion.a target={"_blank"} href="https://github.com/andriankusuma97"
+         className="text-3xl "
+         whileHover={{ y: -3 }}
+         whileTap={{ scale: 0.9 }}
+         > <BsGithub/> </motion.a>
+        <motion.a target={"_blank"} href="https://www.linkedin.com/in/andrian-kusuma-9150461a2/" className="text-3xl ml-3 "
+         whileHover={{ y: -3 }}
+         whileTap={{ scale: 0.9 }}
+        > <BsLinkedin/> </motion.a>
         
       </div>
     </div>
--- client-portofolio/src/pages/Project.jsx
+++ client-portofolio/src/pages/Project.jsx
@@ -3,14 +3,14 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchData } from "../store/action/actionCreator";
 
-function PortofolioPage() {
+function ProjectPage() {
  
 
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Portofolio Page</h1>
+      <h1 className="text-3xl font-bold underline">Project Page</h1>
     </div>
   );
 }
 
-export default PortofolioPage;
+export default ProjectPage;
